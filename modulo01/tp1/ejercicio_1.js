5f6f53a731e82cd6e7f154b4d1eae1a40719f215
mercedes kurday <136277511+merkurday@users.noreply.github.com>
2023-06-14 21:25:37 -0300

    Add files via upload

---
+++ modulo01/tp1/ejercicio_1.js
@@ -0,0 +1,3 @@
+document.writeln("Hola Mundo!");
+
+//Es lo que se imprime en la pantalla del navegador// 
---
+++ modulo01/tp1/ejercicio_9.js
@@ -0,0 +1,6 @@
+let vocales = [`A`, `E`, `I`, `O`, `U`];
+for(let i= 0; i < vocales.length; i++){
+    document.write(vocales[i]);
+    document.write("</br>");
+}
+
