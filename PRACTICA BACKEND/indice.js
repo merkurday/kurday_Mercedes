0e740406f9748124aa8ffbeca3ab73ced51a2f0e
mercedes kurday <136277511+merkurday@users.noreply.github.com>
2023-10-14 22:28:09 -0300

    Create indice.js

---
+++ PRACTICA BACKEND/indice.js
@@ -0,0 +1,26 @@
+require('rootpath')();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+const configuracion = require("config.json");
+
+const controladorPersona = require("controller/personaController.js");
+const controladorUsuario = require("controller/usuarioController.js");
+// const controladorArticulo = require("controller/articuloController.js");
+
+app.use('/api/persona', controladorPersona);
+app.use('/api/usuario', controladorUsuario);
+// app.use('/articulos', controladorArticulo);
+
+app.listen(configuracion.server.port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("sevidor escuchando en el puerto " + configuracion.server.port);
+    }
+});
